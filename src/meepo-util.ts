11cb488a0d3968a70860b5f73670311fda7e80fa
15138730370 <1037483576@qq.com>
2018-01-06 14:24:38 +0800

    测试文件

--- src/meepo-util.ts
+++ src/meepo-util.ts
@@ -1,106 +1,101 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-// import "core-js/fn/array.find"
-// ...
-export class Util {
-  clamp(min: number, n: number, max: number) {
-    return Math.max(min, Math.min(n, max));
-  }
+export function clamp(min: number, n: number, max: number) {
+  return Math.max(min, Math.min(n, max));
+}
 
-  deepCopy(obj: any) {
-    return JSON.parse(JSON.stringify(obj));
-  }
+export function deepCopy(obj: any) {
+  return JSON.parse(JSON.stringify(obj));
+}
 
-  deepEqual(a: any, b: any) {
-    if (a === b) {
-      return true;
-    }
-    return JSON.stringify(a) === JSON.stringify(b);
+export function deepEqual(a: any, b: any) {
+  if (a === b) {
+    return true;
   }
-  /**
-   * 是否为boolean类型
-   * @param val 
-   */
-  isBoolean(val: any): val is boolean { return typeof val === 'boolean'; }
-  /**
-   * 是否为string类型
-   * @param val 
-   */
-  isString(val: any): val is string { return typeof val === 'string'; }
-  /**
-   * 是否为number类型
-   * @param val 
-   */
-  isNumber(val: any): val is number { return typeof val === 'number'; }
-  /**
-   * 是否为function类型
-   * @param val 
-   */
-  isFunction(val: any): val is Function { return typeof val === 'function'; }
-  /**
-   * 不是undefined类型
-   * @param val 
-   */
-  isDefined(val: any): boolean { return typeof val !== 'undefined'; }
-  /**
-   * 是undefined
-   * @param val 
-   */
-  isUndefined(val: any): val is undefined { return typeof val === 'undefined'; }
-  /**
-   * 不为undefined 并且 不为null
-   * @param val 
-   */
-  isPresent(val: any): val is any { return val !== undefined && val !== null; }
-  /**
-   * 是undefined或者是null
-   * @param val 
-   */
-  isBlank(val: any): val is null { return val === undefined || val === null; }
-  /**
-   * 是Object
-   * @param val 
-   */
-  isObject(val: any): val is Object { return typeof val === 'object'; }
-  /**
-   * 是Array
-   * @param val 
-   */
-  isArray(val: any): val is any[] { return Array.isArray(val); }
+  return JSON.stringify(a) === JSON.stringify(b);
+}
+/**
+ * 是否为boolean类型
+ * @param val 
+ */
+export function isBoolean(val: any): val is boolean { return typeof val === 'boolean'; }
+/**
+ * 是否为string类型
+ * @param val 
+ */
+export function isString(val: any): val is string { return typeof val === 'string'; }
+/**
+ * 是否为number类型
+ * @param val 
+ */
+export function isNumber(val: any): val is number { return typeof val === 'number'; }
+/**
+ * 是否为function类型
+ * @param val 
+ */
+export function isFunction(val: any): val is Function { return typeof val === 'function'; }
+/**
+ * 不是undefined类型
+ * @param val 
+ */
+export function isDefined(val: any): boolean { return typeof val !== 'undefined'; }
+/**
+ * 是undefined
+ * @param val 
+ */
+export function isUndefined(val: any): val is undefined { return typeof val === 'undefined'; }
+/**
+ * 不为undefined 并且 不为null
+ * @param val 
+ */
+export function isPresent(val: any): val is any { return val !== undefined && val !== null; }
+/**
+ * 是undefined或者是null
+ * @param val 
+ */
+export function isBlank(val: any): val is null { return val === undefined || val === null; }
+/**
+ * 是Object
+ * @param val 
+ */
+export function isObject(val: any): val is Object { return typeof val === 'object'; }
+/**
+ * 是Array
+ * @param val 
+ */
+export function isArray(val: any): val is any[] { return Array.isArray(val); }
 
-  isPrimitive(val: any) {
-    return this.isString(val) || this.isBoolean(val) || (this.isNumber(val) && !isNaN(val));
-  }
+export function isPrimitive(val: any) {
+  return isString(val) || isBoolean(val) || (isNumber(val) && !isNaN(val));
+}
 
-  isTrueProperty(val: any): boolean {
-    if (typeof val === 'string') {
-      val = val.toLowerCase().trim();
-      return (val === 'true' || val === 'on' || val === '');
-    }
-    return !!val;
+export function isTrueProperty(val: any): boolean {
+  if (typeof val === 'string') {
+    val = val.toLowerCase().trim();
+    return (val === 'true' || val === 'on' || val === '');
   }
+  return !!val;
+}
 
-  isCheckedProperty(a: any, b: any): boolean {
-    if (a === undefined || a === null || a === '') {
-      return (b === undefined || b === null || b === '');
-    } else if (a === true || a === 'true') {
-      return (b === true || b === 'true');
-    } else if (a === false || a === 'false') {
-      return (b === false || b === 'false');
-    } else if (a === 0 || a === '0') {
-      return (b === 0 || b === '0');
-    }
-    return (a === b);
+export function isCheckedProperty(a: any, b: any): boolean {
+  if (a === undefined || a === null || a === '') {
+    return (b === undefined || b === null || b === '');
+  } else if (a === true || a === 'true') {
+    return (b === true || b === 'true');
+  } else if (a === false || a === 'false') {
+    return (b === false || b === 'false');
+  } else if (a === 0 || a === '0') {
+    return (b === 0 || b === '0');
   }
+  return (a === b);
+}
 
-  reorderArray(array: any[], indexes: { from: number, to: number }): any[] {
-    const element = array[indexes.from];
-    array.splice(indexes.from, 1);
-    array.splice(indexes.to, 0, element);
-    return array;
-  }
+export function reorderArray(array: any[], indexes: { from: number, to: number }): any[] {
+  const element = array[indexes.from];
+  array.splice(indexes.from, 1);
+  array.splice(indexes.to, 0, element);
+  return array;
+}
 
-  removeArrayItem(array: any[], item: any) {
-    const index = array.indexOf(item);
-    return !!~index && !!array.splice(index, 1);
-  }
+export function removeArrayItem(array: any[], item: any) {
+  const index = array.indexOf(item);
+  return !!~index && !!array.splice(index, 1);
 }
--- test/meepo-util.test.ts
+++ test/meepo-util.test.ts
@@ -1,14 +1,111 @@
-import DummyClass from "../src/meepo-util"
+import * as util from "../src/meepo-util"
 
 /**
  * Dummy test
  */
-describe("Dummy test", () => {
+describe("Util test", () => {
+
   it("works if true is truthy", () => {
     expect(true).toBeTruthy()
   })
 
-  it("DummyClass is instantiable", () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  it("Util clamp", () => {
+    let r = util.clamp(1, 2, 3);
+    expect(r).toEqual(2);
+  })
+
+  it('Util deepCopy', () => {
+    let d = { title: '1' };
+    let t = util.deepCopy(d);
+    expect(t).toEqual({ title: '1' })
+  })
+
+  it('Util deepEqual', () => {
+    let d1 = { title: { t: 1 } };
+    let d2 = { title: { t: '1' } };
+    let d3 = { title: { t: '1' } };
+    expect(util.deepEqual(d1, d2)).toEqual(false);
+    expect(util.deepEqual(1, 1)).toEqual(true);
+  })
+
+  it('Util isBoolean', () => {
+    expect(util.isBoolean('false')).toEqual(false);
+  })
+
+  it('Util isString', () => {
+    expect(util.isString('false')).toEqual(true);
+  })
+
+  it('Util isNumber', () => {
+    expect(util.isNumber(1)).toEqual(true);
+  })
+
+  it('Util isFunction', () => {
+    expect(util.isFunction(1)).toEqual(false);
+  })
+
+  it('Util isDefined', () => {
+    expect(util.isDefined(1)).toEqual(true);
+  })
+
+  it('Util isUndefined', () => {
+    expect(util.isUndefined(1)).toEqual(false);
+  })
+
+  it('Util isPresent', () => {
+    expect(util.isPresent(1)).toEqual(true);
+    let item;
+    expect(util.isPresent(item)).toEqual(false);
+  })
+
+  it('Util isBlank', () => {
+    expect(util.isBlank(1)).toEqual(false);
+  })
+
+  it('Util isObject', () => {
+    expect(util.isObject(1)).toEqual(false);
+  })
+
+  it('Util isArray', () => {
+    expect(util.isArray([1])).toEqual(true);
+  })
+
+  it('Util isPrimitive', () => {
+    expect(util.isPrimitive(1)).toEqual(true);
+  })
+
+  it('Util isTrueProperty', () => {
+    expect(util.isTrueProperty('')).toEqual(true);
+    expect(util.isTrueProperty('true')).toEqual(true);
+    expect(util.isTrueProperty('on')).toEqual(true);
+    expect(util.isTrueProperty(false)).toEqual(false);
+  })
+
+  it('Util isCheckedProperty', () => {
+    expect(util.isCheckedProperty('true', true)).toEqual(true);
+    expect(util.isCheckedProperty(undefined, undefined)).toEqual(true);
+    expect(util.isCheckedProperty(undefined, null)).toEqual(true);
+    expect(util.isCheckedProperty(undefined, '')).toEqual(true);
+
+    expect(util.isCheckedProperty(true, 'true')).toEqual(true);
+    expect(util.isCheckedProperty('true', true)).toEqual(true);
+
+    expect(util.isCheckedProperty(false, 'false')).toEqual(true);
+    expect(util.isCheckedProperty('false', false)).toEqual(true);
+
+    expect(util.isCheckedProperty('0', 0)).toEqual(true);
+    expect(util.isCheckedProperty(0, '0')).toEqual(true);
+
+    expect(util.isCheckedProperty(1, 1)).toEqual(true);
+  })
+
+  it('Util reorderArray', () => {
+    let items = [1, 2, 3, 4, 5];
+    expect(util.reorderArray(items, { from: 1, to: 3 })).toEqual([1, 3, 4, 2, 5]);
+  })
+
+  it('Util removeArrayItem', () => {
+    let items = [1, 2];
+    expect(util.removeArrayItem(items, 1)).toEqual(true);
   })
 })
