8b9b0099d4ac7afaed4bcabd636eb596c55364bf
15138730370 <1037483576@qq.com>
2018-01-06 16:52:32 +0800

    保存

--- src/meepo-util.ts
+++ src/meepo-util.ts
@@ -1,3 +1,5 @@
+const _: any = require('underscore');
+
 export function clamp(min: number, n: number, max: number) {
   return Math.max(min, Math.min(n, max));
 }
@@ -12,36 +14,8 @@ export function deepEqual(a: any, b: any) {
   }
   return JSON.stringify(a) === JSON.stringify(b);
 }
-/**
- * 是否为boolean类型
- * @param val 
- */
-export function isBoolean(val: any): val is boolean { return typeof val === 'boolean'; }
-/**
- * 是否为string类型
- * @param val 
- */
-export function isString(val: any): val is string { return typeof val === 'string'; }
-/**
- * 是否为number类型
- * @param val 
- */
-export function isNumber(val: any): val is number { return typeof val === 'number'; }
-/**
- * 是否为function类型
- * @param val 
- */
-export function isFunction(val: any): val is Function { return typeof val === 'function'; }
-/**
- * 不是undefined类型
- * @param val 
- */
+
 export function isDefined(val: any): boolean { return typeof val !== 'undefined'; }
-/**
- * 是undefined
- * @param val 
- */
-export function isUndefined(val: any): val is undefined { return typeof val === 'undefined'; }
 /**
  * 不为undefined 并且 不为null
  * @param val 
@@ -52,19 +26,9 @@ export function isPresent(val: any): val is any { return val !== undefined && va
  * @param val 
  */
 export function isBlank(val: any): val is null { return val === undefined || val === null; }
-/**
- * 是Object
- * @param val 
- */
-export function isObject(val: any): val is Object { return typeof val === 'object'; }
-/**
- * 是Array
- * @param val 
- */
-export function isArray(val: any): val is any[] { return Array.isArray(val); }
 
 export function isPrimitive(val: any) {
-  return isString(val) || isBoolean(val) || (isNumber(val) && !isNaN(val));
+  return _.isString(val) || _.isBoolean(val) || (_.isNumber(val) && !isNaN(val));
 }
 
 export function isTrueProperty(val: any): boolean {
